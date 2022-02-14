cbf4070438fd241f539bda96e97f13c22e651554
harsha <harsha478478@gmail.com>
2022-02-14 11:23:23 +0530

    Assisted Projects

---
+++ Assisted Projects/cookiedemo/src/main/java/com/simplilearm/cookie/LoginServlet.java
@@ -0,0 +1,42 @@
+package com.simplilearm.cookie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String name= request.getParameter("userid");
+		String pass= request.getParameter("password");
+		
+		Cookie cookie= new Cookie("userid", name);
+		Cookie cookie1= new Cookie("password", pass);
+		response.addCookie(cookie);
+		response.addCookie(cookie1);
+		
+		//response.getWriter().print("<a href='dashboard'>Dashboard</a>");
+		response.sendRedirect("dashboard");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
